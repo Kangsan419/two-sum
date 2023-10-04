53cc80b89153d1972ac843ae6b53cd3154e60815
kangsan <rkdtks419@g.skku.edu>
2023-10-04 18:52:04 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#this is for OSSP 6th week assignment
+
+"""
+Contents
+- Return the indices of two integers from the given array "nums" that add up
+to the specified integer "target"
+- Assume that only one possible solution exists, and each array element can only be used once.
+(cannot be used twice or more times.)
+- You can return the answer in any order    
+"""
+
+from typing import List
+def twoSum(self,nums: List[int], target: int) -> List[int]:
+    # YOUR ANSWER
+    #무식하게 한번 쭉 둘러보라고 하면 되지 않을까?
+    for i in range(0, len(nums)):
+        #i랑 중복 방지
+        for j in range(i+1, len(nums)):
+            #조건문
+	        if nums[i] + nums[j] == target:
+	            return [i, j]
+         
+    
+
+
